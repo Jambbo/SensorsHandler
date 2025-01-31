71c3c38ecb916128c4bcdba5100147fd6e1d8db5
Gleb <chibgleb99@gmail.com>
2025-01-31 19:56:05 +0000

    implemented the logic with saving the data from kafka to database

--- data-analyser-microservice/src/main/java/com/example/dataanalysermicroservice/model/Data.java
+++ data-analyser-microservice/src/main/java/com/example/dataanalysermicroservice/model/Data.java
@@ -1,5 +1,6 @@
 package com.example.dataanalysermicroservice.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,11 +12,18 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @ToString
+@Entity
+@Table(name = "data")
 public class Data {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private Long sensorId;
     private LocalDateTime timestamp;
     private double measurement;
+    @Column(name = "type")
+    @Enumerated(value = EnumType.STRING)
     private MeasurementType measurementType;
 
     public enum MeasurementType {
---
+++ data-analyser-microservice/src/main/resources/liquibase/changesets/001_create_tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE data
+(
+    id          BIGSERIAL PRIMARY KEY,
+    sensor_id   BIGINT    NOT NULL,
+    timestamp   TIMESTAMP NOT NULL,
+    measurement FLOAT     NOT NULL,
+    type        VARCHAR   NOT NULL
+
+)
